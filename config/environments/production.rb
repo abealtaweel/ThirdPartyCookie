d08c5de15f7840d94e5016733922d2ca00bb81aa
Ibrahim Altaweel <Abe@Abes-MacBook-Pro.local>
2014-06-20 17:23:49 -0700

    bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-ThirdPartyCookie::application.configure do
+ThirdPartyCookie::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
